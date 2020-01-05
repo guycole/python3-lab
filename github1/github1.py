c67bf6ba130ceb6440ab47b2fa007a5887127452
Guy Cole <guycole@gmail.com>
2020-01-05 14:03:52 -0800

    add pagination

--- github1/github1.py
+++ github1/github1.py
@@ -12,7 +12,27 @@ class Harvester:
     extract issues from a github repository
     """
 
-    def comments(self, buffer):
+
+    def repo(self, owner, auth_tuple):
+        """
+        return a list of repositories
+        :param owner:
+        :param auth_tuple:
+        :return:
+        """
+        results = []
+        url = f"https://api.github.com/orgs/{owner}/repos?type=all"
+
+        response = requests.get(url, auth=auth_tuple)
+        print(response.content)
+        if response.status_code == 200:
+            for row in response.json():
+                results.append(row['name'])
+
+        # NOTE: does not return all the expected repositories (only 30 for scoop, vs 200 promised
+        return results
+
+    def comment(self, buffer):
         """
         parse github issue comments
         :param buffer: raw comments
@@ -59,7 +79,7 @@ class Harvester:
             if comment_population > 0:
                 response = requests.get(candidate['comments_url'], auth=auth_tuple)
                 if response.status_code == 200:
-                    comments = self.comments(response.json())
+                    comments = self.comment(response.json())
 
             result['comments'] = comments
 
@@ -82,7 +102,7 @@ class Harvester:
         :param rows: extracted github issues
         :return: None
         """
-        print(f"writing {file_name}")
+        print(f"writing {file_name} with row population {len(rows)}")
         with open(file_name, mode='w') as csv_file:
             fieldnames = ['number', 'title', 'reporter', 'assigned', 'created_at', 'updated_at', 'body', 'comments', 'labels']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -90,6 +110,45 @@ class Harvester:
             writer.writeheader()
             for row in rows:
                 writer.writerow(row)
+
+
+    def paginator(self, headers):
+        """
+        discover if this repository paginates for issues
+        :param headers: raw response header
+        :return: tuple in the form (next_page, max_page, base_url)
+        """
+        base_url = ''
+        min_ndx = 0
+        max_ndx = 0
+
+        if 'Link' in headers:
+            link = headers['Link']
+            #
+            # https://developer.github.com/v3/guides/traversing-with-pagination/
+            #
+            # if link is present, there is pagination and results look like this
+            # <https://api.github.com/repositories/33013398/issues?per_page=100&page=2>; rel="next",
+            # <https://api.github.com/repositories/33013398/issues?per_page=100&page=3>; rel="last"
+            #
+            # Discover 'base' URL and min, max additional pages
+            #
+            split1 = link.split(';')
+            next_page = split1[0]
+            last_page = split1[1]
+
+            ndx1 = next_page.rfind('=')
+            ndx2 = next_page.rfind('>')
+            base_url = next_page[1:ndx1+1]
+            # base_url = https://api.github.com/repositories/33013398/issues?per_page=100&page=
+            min_ndx = next_page[ndx1+1:ndx2]
+
+            ndx1 = last_page.rfind('=')
+            ndx2 = last_page.rfind('>')
+            max_ndx = last_page[ndx1+1:ndx2]
+
+        return (int(min_ndx), int(max_ndx), base_url)
+
 
     def execute(self, user, password, owner, repository):
         """
@@ -102,15 +161,38 @@ class Harvester:
         """
         auth_tuple = (user, password)
         header = {"Accept": "application/vnd.github.full+json"}
-        url = f"https://api.github.com/repos/{owner}/{repository}/issues"
 
+        rows = []
+        print("page count:1")
+
+        # initial remote read
+        url = f"https://api.github.com/repos/{owner}/{repository}/issues?per_page=100"
         response = requests.get(url, auth=auth_tuple, headers=header)
         if response.status_code == 200:
             print(f"successful read from github repository {repository}")
-            rows = self.parser(response.json(), auth_tuple)
-            self.writer(f"{repository}.csv", rows)
+            rows += self.parser(response.json(), auth_tuple)
+
+            # might paginate and require additional visits
+            page_tuple = self.paginator(response.headers)
+            if len(page_tuple[2]) > 0:
+                for page_ndx in range(page_tuple[0], 1+page_tuple[1]):
+                    print(f"page count:{page_ndx} of {page_tuple[1]}")
+                    url = f"{page_tuple[2]}{page_ndx}"
+
+                    response = requests.get(url, auth=auth_tuple, headers=header)
+                    if response.status_code == 200:
+                        print(f"successful read from github repository {repository}")
+                        rows += self.parser(response.json(), auth_tuple)
+                    else:
+                        print(f"bad status {response.status_code} from github repository {repository}")
         else:
             print(f"bad status {response.status_code} from github repository {repository}")
+
+        if len(rows) > 0:
+            self.writer(f"{repository}.csv", rows)
+        else:
+            print("skipping output file")
+
 
 print("start")
 
