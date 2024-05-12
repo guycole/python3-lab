d55e63e4bbfe2460043f77f4019487471802010d
Guy Cole <guycole@gmail.com>
2024-05-12 16:29:21 -0700

    tweak mountain_array

--- interview/mountain_array.py
+++ interview/mountain_array.py
@@ -10,7 +10,7 @@ from typing import List
 class Solution:
 
     def execute(self, candidates: List[int]) -> bool:
-        print("execute")
+        print(f"execute {candidates}")
 
         if len(candidates) < 3:
             return False
@@ -27,9 +27,8 @@ class Solution:
         print(f"peak noted {peak_ndx} {peak_value}")
 
         for ndx1 in range(ndx1+1, len(candidates)-1):
-            if candidates[ndx1] > candidates[ndx1+1]:
-                pass
-            else:
+            if candidates[ndx1] < candidates[ndx1+1]:
+                print(f"fail early at {ndx1}")
                 return False
 
         if peak_ndx < 0:
@@ -41,9 +40,9 @@ if __name__ == '__main__':
     print("main")
 
     solution = Solution()
-    print(solution.execute([1, 3, 2]))
-    print(solution.execute([1, 2, 3, 2, 1]))
-    print(solution.execute([1, 2, 3, 2, 1, 4]))
+    print(solution.execute([3, 5, 5])) # false
+    print(solution.execute([0, 3, 2, 1])) # true
+    print(solution.execute([0, 2, 3, 4, 5, 2, 1])) # true
 
 #;;; Local Variables: ***
 #;;; mode:python ***
