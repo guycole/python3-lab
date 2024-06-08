e4a3507a44051601922a0ff735b16645b1d0ea44
Guy Cole <guycole@gmail.com>
2024-06-07 23:39:33 -0700

    working heapsort

--- interview/heap_sort.py
+++ interview/heap_sort.py
@@ -1,26 +1,70 @@
 #
 # Title: heap_sort.py
-# Description: 
-# time complexity O(nlogn)
+# Description: build heap then remove in order
+# time complexity O(n logn) slower than quickwortt
 # space complexity O(1)
 #
 # 1. build max heap
+# 2. extract element from heap in sorted order
+#
 
 
 class Solution:
 
-    def execute(self, candiddates: list[int]) -> list[int]:
-        print("execute")
+    def heapify(self, heap:list[int], limit:int, ndx:int) -> None:
+        #print(f"heapify {heap} {limit} {ndx}")
+        maximum = ndx
+
+        left = 2 * ndx + 1
+        right = 2 * ndx + 2
+
+        # if left child exists
+        if left < limit and heap[ndx] < heap[left]:
+            maximum = left
+
+        # if right child exists
+        if right < limit and heap[maximum] < heap[right]:
+            maximum = right
+
+        # root
+        if maximum != ndx:
+            #print(f"swap {heap[ndx]} {heap[maximum]}")
+            heap[ndx], heap[maximum] = heap[maximum], heap[ndx] # swap root
+            self.heapify(heap, limit, maximum)
+
+    def heap_sort(self, heap:list[int]) -> None:
+        print(f"start {heap}")
+
+        limit = len(heap)
+
+        # maxheap
+        for ndx in range(limit, -1, -1):
+            self.heapify(heap, limit, ndx)
+
+        print(f"maxheap {heap}")
+
+        # element extraction
+        for ndx in range(limit-1, 0, -1):
+            heap[ndx], heap[0] = heap[0], heap[ndx]
+            self.heapify(heap, ndx, 0)
+
+        print(f"extract {heap}")
+
+    def execute(self, candidates: list[int]) -> None:
+        print(f"execute {candidates}")
+
+        self.heap_sort(candidates)
+        print(candidates)
 
         results = []
-
-        return results
 
 if __name__ == '__main__':
     print("main")
 
     solution = Solution()
     solution.execute([12, 3, 9, 14, 10, 18, 8, 23])
+
+    heap = [4, 3, 1, 0, 2]
 
 #;;; Local Variables: ***
 #;;; mode:python ***
--- interview/quick_sort.py
+++ interview/quick_sort.py
@@ -1,26 +1,35 @@
 #
 # Title: quick_sort.py
 # Description: 
-# time complexity O(n logn)
-# space complexity O(1)
+# "divide and conquer strategy"
+# partition to sub arrays w/a pivot element
+# pivot element is crucial to performance
+# recursively sort the sub arrays (bad for large array)
+# recursion stop when sub array has < 2 elements
+# average time complexity O(n logn) worst is O(n^2) when pivot is smallest or largest element
+# space complexity O(log n) for recursion worst is O(n)
 #
 
 
 class Solution:
  
+    # returns partition index
     def partition(self, array:list[int], low:int, high:int) -> int:
-        pivot = array[high]
+        pivot = array[high] # last element
         print(f"pivot {pivot} {low} {high}")
 
         ii = low - 1
         for jj in range(low, high):
             if array[jj] <= pivot:
+                # move to left partition
                 ii += 1
                 array[ii], array[jj] = array[jj], array[ii]
                 print(f"swap ii and jj {ii} {jj} {array}")
 
+        # move pivot to correct position
         array[ii + 1], array[high] = array[high], array[ii + 1]
 
+        # return pivot index
         return ii + 1
 
     def quick_sort(self, candidates:list[int], low:int, high:int) -> None:
