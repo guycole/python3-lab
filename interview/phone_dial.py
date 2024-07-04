d2126cd850a0709162216449cb79f1181b4566de
Guy Cole <guycole@gmail.com>
2024-07-04 11:53:09 -0700

    updated phone_dial

--- interview/phone_dial.py
+++ interview/phone_dial.py
@@ -4,56 +4,6 @@
 #
 
 class Solution:
-    def converter(self, candidates: List[str], indices: List[int]) -> str:
-        result = ""
-
-        for ndx1 in range(len(indices)):
-            result = result + candidates[ndx1][indices[ndx1]]
-
-        return result
-    
-    def sequencer(self, candidates: List[str]) -> List[int]:
-        print(candidates)
-
-        limit = []
-        total = 1
-        for candidate in candidates:
-            limit.append(len(candidate))
-            total = total * len(candidate) 
-#        print(limit)
-#        print(total)
-
-        current = [0]*len(candidates)
-
-        indices = []
-        indices.append(current.copy())
-
-        for ndx1 in range(total):
-            indices.append(current.copy())
-    
-        for ndx1 in range(1, total):
-#            print(current)
-
-            carry = 1
-            digit_ndx = len(candidates)
-
-            while carry > 0:
-                digit_ndx = digit_ndx - 1
-#                print(f"incrementing: {digit_ndx}")
-
-                temp = current[digit_ndx]
-                temp = temp + 1
-
-                if temp >= limit[digit_ndx]:
-                    carry = 1
-                    current[digit_ndx] = 0
-                else:
-                    carry = 0
-                    current[digit_ndx] = temp
-
-                indices[ndx1] = current.copy()
-
-        return indices
 
     def letterCombinations(self, digits: str) -> list[str]:
         keys = {"0": "", "1":"", "2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
@@ -64,53 +14,54 @@ class Solution:
         if len(digits) < 2:
             return(list(keys[digits[0]]))
 
-        digitz = []
-        characterz = []
+        digitz = [] # digits with characters 
+        characterz = [] # characters for each digit
         for ndx1 in range(len(digits)):
             temp = keys[digits[ndx1]]
             if len(temp) > 0:
                 digitz.append(digits[ndx1])
                 characterz.append(temp)
 
+        # each character entry has an index
         indicez = [0] * len(digitz)
 
         print(digitz)
         print(indicez)
         print(characterz)
 
-#        indices = []
-#        for ndx1 in range(len(digits)):
-#            indices.append(0)
-
-
-#        candidates = []
-#        for ndx1 in list(digits):
-#            if len(keys[ndx1]) > 0:
-#                candidates.append(keys[ndx1])
-
-#        print(candidates)
-
-#        indices = self.sequencer(candidates)  
-#        print(indices)
-#        print(len(indices))
-
         results = []
-#        for ndx in indices:
-#            temp = self.converter(candidates, ndx)
-#            if temp not in results:
-#                results.append(temp)
 
-#        print(len(results))
+        while True:
+            # print(indicez)
+
+            # build a string from the indices
+            buffer = []
+            for temp in range(len(indicez)):
+                buffer.append(characterz[temp][indicez[temp]])
+#                print(buffer)
+
+            results.append("".join(buffer))
+
+            # index management
+            for temp in range(len(indicez)-1 , -1, -1):
+                indicez[temp] = indicez[temp] + 1
+                if indicez[temp] >= len(characterz[temp]):
+                    indicez[temp] = 0
+                else:
+                    break
+
+            if sum(indicez) == 0:
+                break
 
         return results
 
 if __name__ == '__main__':
     solution = Solution()
     #print(solution.letterCombinations("2"))
-    print(solution.letterCombinations("234"))
+    #print(solution.letterCombinations("234"))
     #print(solution.letterCombinations("3910057"))
-    #results = solution.letterCombinations("4085936964")
-    #print(results)
+    results = solution.letterCombinations("5936963")
+    print(results)
 
 #;;; Local Variables: ***
 #;;; mode:python ***
