c1b514dcb3f73b2b8166b83cdbbcffba0ec06eb3
Guy Cole <guycole@gmail.com>
2024-08-26 00:43:00 -0700

    updated bintree

--- binloc/driver.py
+++ binloc/driver.py
@@ -6,16 +6,49 @@
 
 class Driver:
 
-    def execute(self, file_name: str) -> None:
-        print("execute")
+  def __init__(self):
+    self.dd = {}
 
-        return
+  # convert from decimal degrees
+  def dd_key(self, rawbuff:str) -> str:
+    tokens = rawbuff.split('.')
+
+    element1 = tokens[1][0:2]
+    element2 = tokens[1][2:4]
+
+    result = f"{tokens[0]}_{element1}_{element2}"
+    return result
+
+  def parser(self, rawbuff:str):
+    tokens = rawbuff.strip().split(' ')
+    if len(tokens) != 2:
+      print("fail")
+
+    key_lat = self.dd_key(tokens[0])
+    key_lng = self.dd_key(tokens[1])
+
+    fresh_key = f"{key_lat}/{key_lng}"
+
+    self.dd[fresh_key] = (tokens[0], tokens[1])
+
+  def execute(self, file_name: str) -> None:
+    print("execute")
+
+    try:
+      with open(file_name, 'r') as infile:
+        buffer = infile.readlines()
+        for element in buffer:
+          self.parser(element)
+    except Exception as error:
+      print(error)
+
+    print(self.dd)
 
 if __name__ == '__main__':
-    print("main")
+  print("main")
 
-    driver = Driver()
-    driver.execute("file1.dat")
+  driver = Driver()
+  driver.execute("file1.dat")
 
 #;;; Local Variables: ***
 #;;; mode:python ***
--- interview/bintree.py
+++ interview/bintree.py
@@ -8,19 +8,21 @@ class Node:
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return f"{self.val} {self.left} {self.right}"
+
     def getsum(self) -> int:
         sum = 0
 
-        while self.left is not None:
-            self = self.left
-            sum += self.val
+        if self.left is not None:
+            sum += self.left.getsum()
+        
+        sum += self.val
 
-        while self.right is not None:
-            self = self.right
-            sum += self.val
+        if self.right is not None:
+            sum += self.right.getsum()
 
         return sum
-
 
 print('start')
 if __name__ == '__main__':
@@ -29,7 +31,10 @@ if __name__ == '__main__':
     node1 = Node(val=1)
     node2 = Node(val=2)
     node3 = Node(val=3, left=node1, right=node2)
+    node4 = Node(val=4)
+    node5 = Node(val=5, left=node4, right=node3)
 
     print(node3.getsum()) # 6
+    print(node5.getsum()) # 15
 
 print('stop')
