878031d053026bcd9efa5675a99d4df1de293c5c
Guy Cole <guycole@gmail.com>
2024-11-04 09:52:52 -0800

    basic verifier

--- choral_wave/verifier.py
+++ choral_wave/verifier.py
@@ -2,16 +2,35 @@
 # Title: verifier.py
 # Description: 
 # 
+import os
+
 from model import Album, Artist, Parser
 
 class Verifier:
 
+    def track_verify(self, album: Album) -> bool:
+        retflag = True
+
+        for song in album.songs:
+            target = f"choral_wave/{song.file_name}"
+
+            if not os.path.exists(target):
+                print(f"file not found: {target}")
+                retflag = False
+
+        return retflag
+
     def execute(self, file_name: str) -> None:
-        print("execute")
+        print("verify: ", file_name)
 
         parser = Parser()
         album = parser.reader(file_name)
-        print(album)
+        print(f"Album: {album.title}, {album.artist.last_name}, {album.release}")
+
+        for song in album.songs:
+            print(f"Song: {song.title}, {song.duration}, {song.file_name}") 
+
+        self.track_verify(album)
 
         return
 
