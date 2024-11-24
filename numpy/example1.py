fb3d5d430793c3e3b1e7bf7559bb74442d4e7c21
Guy Cole <guycole@gmail.com>
2024-11-23 22:27:40 -0800

    numpy

---
+++ numpy/example1.py
@@ -0,0 +1,21 @@
+#
+# Title: example1.py
+# Description: first numpy example
+# 
+import numpy as np
+
+# Create a 2D array
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Print the array
+print(arr)
+
+# Calculate the mean of the array
+mean = np.mean(arr)
+
+# Print the mean
+print(mean)
+
+#;;; Local Variables: ***
+#;;; mode:python ***
+#;;; End: ***
