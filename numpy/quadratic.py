e8d641ecfa3a32376a7a97a1069b26a1b7e9efe6
Guy Cole <guycole@gmail.com>
2024-12-24 14:07:07 -0800

    updated choral wave page docs

---
+++ numpy/quadratic.py
@@ -0,0 +1,16 @@
+#
+# Title: quadratic.py
+# Description: solve ax^2 + bx + c = 0
+# 
+import numpy as np
+
+a = 1
+b = -3
+c = 2
+
+roots = np.roots([a, b, c])
+print(roots)
+
+#;;; Local Variables: ***
+#;;; mode:python ***
+#;;; End: ***
